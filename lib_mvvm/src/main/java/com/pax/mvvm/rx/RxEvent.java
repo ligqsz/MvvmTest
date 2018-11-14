51c2fdc8f7c5e3f4cac85182bf7fdaf521d1eb03
young <ligq@paxsz.com>
2018-11-14 15:57:28 +0800

    优化RxEvent

--- lib_mvvm/src/main/java/com/pax/mvvm/rx/RxEvent.java
+++ lib_mvvm/src/main/java/com/pax/mvvm/rx/RxEvent.java
@@ -6,8 +6,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 import io.reactivex.Observable;
-import io.reactivex.disposables.CompositeDisposable;
-import io.reactivex.disposables.Disposable;
 import io.reactivex.observers.DisposableObserver;
 
 /**
@@ -17,7 +15,6 @@ import io.reactivex.observers.DisposableObserver;
 public class RxEvent {
     private Map<String, Observable<?>> eventMap;
     private static RxEvent rxEvent;
-    private CompositeDisposable mCompositeDisposable;
     private Map<String, DisposableObserver<?>> mDisposableObserverMap;
 
     private RxEvent() {
@@ -35,6 +32,9 @@ public class RxEvent {
     }
 
     public void post(String key, Observable<?> observable) {
+        if (observable == null) {
+            observable = createEventData(0);
+        }
         if (!eventMap.containsKey(key)) {
             eventMap.put(key, observable);
         }
@@ -45,8 +45,17 @@ public class RxEvent {
         if (eventMap.containsKey(key)) {
             eventMap.remove(key);
         }
-        deleteDisposable(mDisposableObserverMap.get(key));
-        mDisposableObserverMap.remove(key);
+        disposableMap(key);
+    }
+
+    private void disposableMap(String key) {
+        DisposableObserver<?> disposableObserver = mDisposableObserverMap.get(key);
+        if (disposableObserver != null) {
+            if (!disposableObserver.isDisposed()) {
+                disposableObserver.dispose();
+            }
+            mDisposableObserverMap.remove(key);
+        }
     }
 
     private Observable<?> getEvent(String key) {
@@ -69,33 +78,13 @@ public class RxEvent {
                     }
                 });
         mDisposableObserverMap.put(key, disposable);
-        addDisposable(disposable);
     }
 
     public void clear() {
         eventMap.clear();
         mDisposableObserverMap.clear();
-        unDisposable();
     }
 
-    private void addDisposable(Disposable disposable) {
-        if (mCompositeDisposable == null) {
-            mCompositeDisposable = new CompositeDisposable();
-        }
-        mCompositeDisposable.add(disposable);
-    }
-
-    private void unDisposable() {
-        if (mCompositeDisposable != null) {
-            mCompositeDisposable.clear();
-        }
-    }
-
-    private void deleteDisposable(Disposable disposable) {
-        if (mCompositeDisposable != null && disposable != null) {
-            mCompositeDisposable.delete(disposable);
-        }
-    }
 
     public <T> Observable<T> createEventData(T data) {
         return Observable.just(data);
